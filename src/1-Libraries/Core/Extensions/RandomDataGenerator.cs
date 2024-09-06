9cc344c18c61abf1a5b896ee3b505775339a0e42
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-09-06 22:21:49 +0200

    feat: add random data generator

---
+++ src/1-Libraries/Core/Extensions/RandomDataGenerator.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeBlock.DevKit.Core.Extensions;
+
+public static class RandomDataGenerator
+{
+    private static readonly Random _rand = new Random();
+    private const string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static string GetRandomString(int length = 8)
+    {
+        var stringChars = new char[length];
+
+        for (int i = 0; i < stringChars.Length; i++)
+            stringChars[i] = CHARS[_rand.Next(CHARS.Length)];
+
+        return new string(stringChars);
+    }
+
+    public static int GetRandomInt(int min = int.MinValue, int max = int.MaxValue)
+    {
+        return _rand.Next(min, max);
+    }
+
+    public static long GetRandomLong(int min = int.MinValue, int max = int.MaxValue)
+    {
+        return _rand.Next(min, max);
+    }
+
+    public static string GetRandomNumber(int length)
+    {
+        string randomNumber = "";
+
+        for (int i = 0; i < length; i++)
+            randomNumber += GetRandomInt(0, 9);
+
+        return randomNumber;
+    }
+
+    public static DateTime GetRandomDateTime()
+    {
+        DateTime start = DateTime.Now.AddYears(-1);
+        DateTime end = DateTime.Now;
+        int range = (end - start).Days;
+        return start.AddDays(_rand.Next(range));
+    }
+}
