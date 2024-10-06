f1b4d179996c000c12706410e6b613f30eb3ff9e
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-06 23:34:25 +0200

    feat: add an enum extension

--- src/1-Libraries/Core/Extensions/DataAnnotationExtension.cs
+++ src/1-Libraries/Core/Extensions/DataAnnotationExtension.cs
@@ -1,19 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace CodeBlock.DevKit.Core.Extensions;
 
-/// <summary>
-///
-/// </summary>
 public static class DataAnnotationExtension
 {
-    /// <summary>
-    ///
-    /// </summary>
     public static bool Validate<TObject>(this TObject obj, out ICollection<ValidationResult> results)
     {
         results = new List<ValidationResult>();
 
         return Validator.TryValidateObject(obj, new ValidationContext(obj), results, true);
+    }
+
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        return enumValue.GetType().GetMember(enumValue.ToString()).First().GetCustomAttribute<DisplayAttribute>().GetName();
     }
 }
