00f911c82de563a3dbb79ced8570827f39f811d9
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-07 17:00:06 +0200

    fix: validation bug

--- src/2-Modules/Authorization/UseCases/UpdateUser/UpdateUserRequest.cs
+++ src/2-Modules/Authorization/UseCases/UpdateUser/UpdateUserRequest.cs
@@ -11,10 +11,8 @@ public class UpdateUserRequest : BaseCommand
     public string Id { get; set; }
 
     [Display(Name = nameof(AuthorizationResource.Mobile), ResourceType = typeof(AuthorizationResource))]
-    [Required(ErrorMessageResourceName = nameof(CommonResource.Required), ErrorMessageResourceType = typeof(CommonResource))]
     public string Mobile { get; set; }
 
     [Display(Name = nameof(AuthorizationResource.Email), ResourceType = typeof(AuthorizationResource))]
-    [Required(ErrorMessageResourceName = nameof(CommonResource.Required), ErrorMessageResourceType = typeof(CommonResource))]
     public string Email { get; set; }
 }
