b5cf319745aa66ded77e7ba1119f52fcd852bae7
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-22 22:14:54 +0200

    feat: add loading

--- src/1-Libraries/Web.Blazor.Server/wwwroot/js/component.js
+++ src/1-Libraries/Web.Blazor.Server/wwwroot/js/component.js
@@ -1,5 +1,23 @@
 ﻿window.Helpers = {
     RedirectTo: function (path) {
         window.location = path;
+    },
+    ShowLoading: function () {
+        displayLoading('block');
+    },
+    HideLoading: function () {
+        displayLoading('none');
+    },
+    DisplayLoading: function (display) {
+        displayLoading(display);
+
     }
 };
+
+function displayLoading(display) {
+    $('.spinner-loading').css('display', display);
+}
+
+(function () {
+    displayLoading('none');
+})();
