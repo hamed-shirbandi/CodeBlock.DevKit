a293c7632887dcb9474c0a59df94bede6cb752c1
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-25 20:12:26 +0200

    feat: update role

---
+++ src/2-Modules/Authorization/UseCases/Roles/UpdateRole/UpdateRoleRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CodeBlock.DevKit.Application.Commands;
+using CodeBlock.DevKit.Authorization.Domain;
+using CodeBlock.DevKit.Core.Resources;
+
+namespace CodeBlock.DevKit.Authorization.UseCases.Roles.UpdateRole;
+
+public class UpdateRoleRequest : BaseCommand
+{
+    [Required(ErrorMessageResourceName = nameof(CommonResource.Required), ErrorMessageResourceType = typeof(CommonResource))]
+    public string Id { get; set; }
+
+    [Display(Name = nameof(AuthorizationResource.Mobile), ResourceType = typeof(AuthorizationResource))]
+    [Required(ErrorMessageResourceName = nameof(CommonResource.Required), ErrorMessageResourceType = typeof(CommonResource))]
+    public string Name { get; set; }
+}
---
+++ src/2-Modules/Authorization/UseCases/Roles/UpdateRole/UpdateRoleUseCase.cs
@@ -0,0 +1,33 @@
+﻿using CodeBlock.DevKit.Application.Bus;
+using CodeBlock.DevKit.Application.Commands;
+using CodeBlock.DevKit.Authorization.Domain;
+using CodeBlock.DevKit.Core.Helpers;
+using CodeBlock.DevKit.Core.Resources;
+using MediatR;
+using ApplicationException = CodeBlock.DevKit.Application.Exceptions.ApplicationException;
+
+namespace CodeBlock.DevKit.Authorization.UseCases.Roles.UpdateRole;
+
+public class UpdateRoleUseCase : BaseCommandHandler, IRequestHandler<UpdateRoleRequest, CommandResult>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public UpdateRoleUseCase(IRoleRepository roleRepository, IInMemoryBus inMemoryBus)
+        : base(inMemoryBus)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public async Task<CommandResult> Handle(UpdateRoleRequest request, CancellationToken cancellationToken)
+    {
+        var role = await _roleRepository.GetByIdAsync(request.Id);
+        if (role is null)
+            throw new ApplicationException(string.Format(CommonResource.Not_Found, AuthorizationResource.Role));
+
+        role.Update(_roleRepository, request.Name);
+
+        await _roleRepository.UpdateAsync(role);
+
+        return CommandResult.Create(entityId: role.Id);
+    }
+}
