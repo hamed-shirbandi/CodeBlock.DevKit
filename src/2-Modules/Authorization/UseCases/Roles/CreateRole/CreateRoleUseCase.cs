9740a7bd244139688bcbb163cf08410f9ce5aa9b
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-25 20:12:15 +0200

    feat: create role

---
+++ src/2-Modules/Authorization/UseCases/Roles/CreateRole/CreateRoleUseCase.cs
@@ -0,0 +1,27 @@
+﻿using CodeBlock.DevKit.Application.Bus;
+using CodeBlock.DevKit.Application.Commands;
+using CodeBlock.DevKit.Authorization.Domain;
+using CodeBlock.DevKit.Core.Helpers;
+using MediatR;
+
+namespace CodeBlock.DevKit.Authorization.UseCases.Roles.CreateRole;
+
+public class CreateRoleUseCase : BaseCommandHandler, IRequestHandler<CreateRoleRequest, CommandResult>
+{
+    private readonly IRoleRepository _roleRepository;
+
+    public CreateRoleUseCase(IRoleRepository roleRepository, IInMemoryBus inMemoryBus)
+        : base(inMemoryBus)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    public async Task<CommandResult> Handle(CreateRoleRequest request, CancellationToken cancellationToken)
+    {
+        var role = Role.Create(_roleRepository, request.Name);
+
+        await _roleRepository.AddAsync(role);
+
+        return CommandResult.Create(entityId: role.Id);
+    }
+}
