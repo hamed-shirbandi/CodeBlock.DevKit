e526fddd9edab8e48598bb24010471009484d82d
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-29 22:01:45 +0100

    chore: remove unused code

--- src/4-Examples/BlazorServerApp/Program.cs
+++ src/4-Examples/BlazorServerApp/Program.cs
@@ -1,17 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using CodeBlock.DevKit.Authorization;
 using CodeBlock.DevKit.Authorization.Infrastructure;
-using CodeBlock.DevKit.Authorization.Resources;
 using CodeBlock.DevKit.Authorization.UI.Configuration;
 using CodeBlock.DevKit.Web.Blazor.Server.Configuration;
 
 namespace BlazorServerApp;
-
-public enum TestEnum
-{
-    [Display(Name = nameof(AuthorizationResource.User_Email), ResourceType = typeof(AuthorizationResource))]
-    First,
-}
 
 public class Program
 {
