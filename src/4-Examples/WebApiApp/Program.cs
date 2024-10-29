f237d41e4308b7c4e8d167db022a992078abb1e1
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-10-29 22:02:11 +0100

    feat: web api sample

--- src/4-Examples/WebApiApp/Program.cs
+++ src/4-Examples/WebApiApp/Program.cs
@@ -1,18 +1,26 @@
+using CodeBlock.DevKit.Authorization;
+using CodeBlock.DevKit.Authorization.Infrastructure;
+using CodeBlock.DevKit.Authorization.UI.Configuration;
 using CodeBlock.DevKit.Web.Api.Configuration;
+using CodeBlock.DevKit.Web.Blazor.Server.Configuration;
 
 namespace WebApiApp;
 
 public class Program
 {
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         builder.AddWebApiPreConfigured(handlerAssemblyMarkerType: typeof(Program));
 
+        builder.Services.AddAuthorizationModule(builder.Configuration);
+
         var app = builder.Build();
 
         app.UseWebApiPreConfigured();
+
+        app.Services.InitialAuthorizationDb();
 
         app.Run();
     }
