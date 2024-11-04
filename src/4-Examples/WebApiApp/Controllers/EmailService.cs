ee8e0bb9e98b57d350604c40e566e0efb7a96a55
Hamed Shirbandi <hamed.shirbandi@gmail.com>
2024-11-04 18:08:35 +0100

    feat: add email service

---
+++ src/4-Examples/WebApiApp/Controllers/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace WebApiApp.Controllers;
+
+public class EmailSender
+{
+    private readonly string _smtpServer;
+    private readonly int _smtpPort;
+    private readonly string _smtpUser;
+    private readonly string _smtpPass;
+
+    public EmailSender(string smtpServer, int smtpPort, string smtpUser, string smtpPass)
+    {
+        _smtpServer = smtpServer;
+        _smtpPort = smtpPort;
+        _smtpUser = smtpUser;
+        _smtpPass = smtpPass;
+    }
+
+    public async Task SendEmailAsync(string to, string subject, string body, bool isBodyHtml = true)
+    {
+        using (var client = new SmtpClient(_smtpServer, _smtpPort))
+        {
+            client.Credentials = new NetworkCredential(_smtpUser, _smtpPass);
+            client.EnableSsl = false; // Ensure SSL is enabled
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_smtpUser),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = isBodyHtml,
+            };
+            mailMessage.To.Add(to);
+
+            await client.SendMailAsync(mailMessage);
+        }
+    }
+}
